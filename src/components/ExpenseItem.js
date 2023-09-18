440ea4519f070d267663072edb72f8bba34e38c2
manupuranic <manupuranic@gmail.com>
2023-09-18 22:47:32 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+const ExpenseItem = () => {
+  return (
+    <div>
+      <h3>Expense Item</h3>
+      <ul>
+        <li>Food Rs.10</li>
+        <li>Petrol Rs.100</li>
+        <li>Movies Rs.200</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ExpenseItem;
